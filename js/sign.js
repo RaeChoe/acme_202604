0becd1214f96cc265cdd34640f73761ecd4edf6e
RaeChoe <picasomati@gmail.com>
2026-05-20 16:33:24 +0900

    feat: 피드백 js 추가

---
+++ js/sign.js
@@ -0,0 +1,41 @@
+const inputs = document.querySelectorAll("form input");
+const form = document.querySelector("form");
+const feedback = document.querySelector(".feedback");
+let passwd = document.querySelector("#userpw");
+let passwdCF = document.querySelector("#userpwcf");
+
+form.addEventListener("submit", e => {
+  e.preventDefault();
+  let completed = 0;
+  // 대상.value 입력한 값 확인
+  // input 포커스 적용, 대상.focus()
+  for (let input of inputs) {
+    if (!input.value) {
+      input.focus();
+      let pos = input.offsetTop;
+      let msg = `${input.getAttribute("id")}는 필수입니다.`;
+      openFeedback(pos, msg);
+    } else {
+      ++completed;
+    }
+  }
+  // 비밀번호 일치 확인하기, 틀리면 '비밀번호가 일치하지 않습니다.'
+  if (passwd.value && passwdCF.value) {
+    if (passwd.value !== passwdCF.value) {
+      let pos = passwdCF.parentElement.offsetTop;
+      let msg = "비밀번호가 일치하지 않습니다";
+      openFeedback(pos, msg);
+    } else {
+      closeFeedback();
+    }
+  }
+});
+
+function openFeedback(pos, msg) {
+  feedback.style.top = `${pos}px`;
+  feedback.innerHTML = `<p>${msg}</p>`;
+  feedback.style.visibility = "visible";
+}
+function closeFeedback() {
+  feedback.style.visibility = "hidden";
+}
